6dfaa8946b5bd3bec073f9aac66c164113161506
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-07-07 00:49:46 -0700

    test translations

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
+
+import { translations } from 'locales/i18n';
 
 export function HomePage() {
+  const { t } = useTranslation();
+  const title = t(translations.title());
+  //const formattedString = t(translations.formattedString());
+  const formattedString = t('formattedString', {
+    first: 'my first',
+    second: 'my second',
+  });
+
+  const handleClick = () => {
+    i18next.changeLanguage('de');
+  };
+
   return (
     <>
       <Helmet>
-        <title>Home Page</title>
+        <title>{title} Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <span>HomePage container</span>
+      <span>{title} container</span>
+      <br />
+      <span>{t(translations.newString())}</span>
+      <div>{formattedString}</div>
+      <hr />
+      <button onClick={handleClick}>Switch</button>
     </>
   );
 }
